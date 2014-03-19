c4140d896aa8423ee6af20d25f3922f2507fa0ed
Laci Pauley <lrpaul01@louisville.edu>
2014-03-19 16:11:22 -0400

    Deleted Old Added New
    Notice the Pink dog on the homepage so cute and also the fact that the
    learn more button points to www.ilovedogs.com lol YAY!

---
+++ homework6/PetStoreInventory/PetStoreInventory/Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FDescription { get; set; }
+        public string PDescription { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
